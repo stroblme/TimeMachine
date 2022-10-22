40891cc3cacfc0464518ae5b7ff22de2be1c9f14
stroblme <melvin.strobl@web.de>
2022-10-22 20:41:17 +0200

    minor interface improvements

--- timemachine.py
+++ timemachine.py
@@ -7,7 +7,7 @@ from hachoir.field import writeIntoFile
 from hachoir.metadata import extractMetadata
 
 
-
+GLOBPATTERN = "**/*.MOV"
 
 
 def timewarp(fileinput, fileoutput, dt_year=0, dt_month=0, dt_day=0, dt_hour=0, dt_min=0, dt_sec=0):
@@ -33,15 +33,19 @@ def timewarp(fileinput, fileoutput, dt_year=0, dt_month=0, dt_day=0, dt_hour=0, 
         # metadata = extractMetadata(parser)
         writeIntoFile(editor, fileoutput)
 
+def main():
 
-
-if __name__ == '__main__':
-    input("Press Ctrl-C if you did not created a backup so far or want to cancel at any point!")
+    print("##### Timemachine #####\n")
+    print("Press Ctrl-C if you did not created a backup so far or want to cancel at any point!")
     print("\nThis program will modify the creation and modification date of all *.MOV files in the provided folder.")
-
+    print("\n##### ########### #####\n")
     ctd = False
     while not ctd:
-        folder = input("\nOk, now enter a folder path: \t")
+        try:
+            folder = input("\nFirst, enter a folder path (Enter to use current folder): \t")
+        except KeyboardInterrupt:
+            print("\n\nSorry to see you go!")
+            return
 
         try:
             folder_path = Path(folder)
@@ -50,27 +54,40 @@ if __name__ == '__main__':
             print("Sorry, somehow I cannot understand the provided folder path. Please try again and make sure you give me an absolute path.")
 
 
-    files = folder_path.glob("**/*.MOV")
-    files_list = list(files)
+        files = folder_path.glob(GLOBPATTERN)
+        files_list = list(files)
 
-    print(f"\nI found {len(files_list)} files in the folder, among which are:")
-    for i, f in enumerate(files_list):
-        print(f)
-        if i > 5:
-            break
+        if len(files_list) == 0:
+            print("\nThere are no files in this folder. Let's try again!")
+            ctd=False
+            continue
 
-    input("\nDoes this look ok?")
+        print(f"\nI found {len(files_list)} files in the folder, among which are:")
+        for i, f in enumerate(files_list):
+            print(f)
+            if i > 5:
+                break
+
+        try:
+            input("\nDoes this look ok?")
+        except KeyboardInterrupt:
+            print("\nOk, let's try again.. (Ctrl-C again to quit)\n")
+            ctd=False
 
     print("\nOk, lets get to the fun part. You are now being asked to specify the timeshift applied to each of your files. Please provide only integer values (optionally with '-' sign).")
 
     ctd=False
     while not ctd:
-        dt_year = input("How many years do you want to travel? (Enter to skip year):\t")
-        dt_month = input("How many months do you want to travel? (Enter to skip month):\t")
-        dt_day = input("How many days do you want to travel? (Enter to skip day):\t")
-        dt_hour = input("How many hours do you want to travel? (Enter to skip hour):\t")
-        dt_min = input("How many minutes do you want to travel? (Enter to skip minute):\t")
-        dt_sec = input("How many seconds do you want to travel? (Enter to skip second):\t")
+        try:
+            dt_year = input("How many years do you want to travel? (Enter to skip year):\t")
+            dt_month = input("How many months do you want to travel? (Enter to skip month):\t")
+            dt_day = input("How many days do you want to travel? (Enter to skip day):\t")
+            dt_hour = input("How many hours do you want to travel? (Enter to skip hour):\t")
+            dt_min = input("How many minutes do you want to travel? (Enter to skip minute):\t")
+            dt_sec = input("How many seconds do you want to travel? (Enter to skip second):\t")
+        except KeyboardInterrupt:
+            print("\n\nSorry to see you go!")
+            return
 
         timeshift = [dt_year, dt_month, dt_day, dt_hour, dt_min, dt_sec]
 
@@ -87,7 +104,12 @@ if __name__ == '__main__':
                 break
 
     print(f"\nOk, now the timeshift looks like:\t {timeshift}\n(year, month, day, hour, minute, second)")
-    input("\nIs that correct? Press any key to continue and I'll start processing your files.")
+
+    try:
+        input("\nIs that correct? Press any key to continue and I'll start processing your files.")
+    except KeyboardInterrupt:
+        print("\n\nSorry to see you go!")
+        return
 
     for i, movie_path in enumerate(files_list):
         try:
@@ -97,3 +119,6 @@ if __name__ == '__main__':
             print(f"Something went wrong wile processing file {movie_path}. Will continue..")
 
     print("I'm done! :)")
+
+if __name__ == '__main__':
+    main()
